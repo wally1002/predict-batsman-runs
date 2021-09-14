223d383bfa0a86e7ae30a64a50b919655faf47b1
wally1002 <sankeerthreddy1002@gmail.com>
2021-09-14 14:07:30 +0530

    drive

--- predict.py
+++ predict.py
@@ -4,6 +4,42 @@ import numpy as np
 import pandas as pd
 import pickle
 
+import requests
+
+def download_file_from_google_drive(id, destination):
+    URL = "https://docs.google.com/uc?export=download"
+
+    session = requests.Session()
+
+    response = session.get(URL, params = { 'id' : id }, stream = True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = { 'id' : id, 'confirm' : token }
+        response = session.get(URL, params = params, stream = True)
+
+    save_response_content(response, destination)    
+
+def get_confirm_token(response):
+    for key, value in response.cookies.items():
+        if key.startswith('download_warning'):
+            return value
+
+    return None
+
+def save_response_content(response, destination):
+    CHUNK_SIZE = 32768
+
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(CHUNK_SIZE):
+            if chunk: # filter out keep-alive new chunks
+                f.write(chunk)
+
+file_id = '1DaAv2IkPBQyePQHWqNrgZ9H3iXlYzEwQ'
+destination = 'pipeline.pkl'
+download_file_from_google_drive(file_id, destination)
+
+
 def load_model():
     data = pickle.load(open('pipeline.pkl', 'rb'))
     return data
